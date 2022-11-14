fb67dff6a9e34258283feac20128443445a5b7b1
afifmustofa <114654020+afifmustofa@users.noreply.github.com>
2022-11-14 16:35:07 +0700

    Update inputabsen.php

--- admin/inputabsen.php
+++ admin/inputabsen.php
@@ -1,10 +1,10 @@
 <?php include "session.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>HRIS Poltracking Indonesia</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
@@ -27,7 +27,12 @@
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/stylecss.css" rel="stylesheet">
+
+  <!-- Datatable -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.3.1
@@ -38,14 +43,13 @@
 </head>
 
 <body>
+  <!-- ======= Header ======= -->
+  <?php include "header.php"; ?>
+  <!-- ======= Sidebar ======= -->
+  <?php include "menu.php"; ?>
+  
 
-   <!-- ======= Header ======= -->
-   <?php include "header.php"; ?>
-<!-- ======= Sidebar ======= -->
-<?php include "menu.php"; ?>
-
-<main id="main" class="main">
-
+  <main id="main" class="main">
 <div class="pagetitle">
   <h1>HRIS Poltracking Indonesia</h1>
   <nav>
@@ -59,41 +63,76 @@
 
 <section class="section">
       <div class="row">
-        <div class="col-lg-20">
-
+        <div class="col-lg-10">
         <div class="card">
             <div class="card-body">
-            <form action="simpan-absen.php" method="POST" enctype="multipart/form-data">
+            <form action="inputabsen.php" method="POST" enctype="multipart/form-data">
               <h5 class="card-title">Input Data Karyawan</h5>
+              <?php
+if(isset($_POST['simpan'])){
+$id_absen      = $_POST['id_absen'];  
+$nama          = $_POST['nama'];
+$tanggal       = $_POST['tanggal'];
+$jammasuk     = $_POST['jammasuk'];
+$jam2         = $_POST['jam2'];
+                              
+                              
+
+
+$sql = mysqli_query($koneksi, "SELECT * FROM tb_karyawan WHERE nama='$nama'");
+             if(mysqli_num_rows($sql) == 0){
+				header("Location: dataabsen.php");
+			}
+
+            $query = mysqli_query($koneksi, "INSERT INTO tb_absen (id_absen, nama, tanggal, jammasuk, jam2, status) VALUES ('$id_absen', '$nama', '$tanggal', '$jammasuk', '$jam2', '$status')");
+            if ($query){
+              echo "<script>alert('Selamat $nama, anda sudah absen!'); window.location = 'dataabsen.php'</script>";
+            //echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Data berhasil disimpan.</div>';
+                  }else{
+                    echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Data gagal disimpan, silahkan coba lagi.</div>';
+                          }
+                      }
+          
+?>
 
               <!-- General Form Elements -->
               <form>
               <div class="row mb-3">
                   <!-- <label for="id_absen" class="col-sm-3 col-form-label">Id Absen</label> -->
-                  <div class="col-md-8 col-lg-9">
+                  <div class="col-md-8 col-lg-6">
                     <input name="id_absen" id="id_absen" type="hidden" class="form-control" placeholder="Tidak perlu di isi" value="<?php $a="P"; $b=rand(1000,10000); $c=$a.$b; echo $c; ?>"autofocus="on" readonly="readonly">
                   </div>
                 </div>
-
                 <div class="row mb-3">
-                  <label class="col-md-4 col-lg-3 col-form-label">Nama Karyawan</label>
-                  <div class="col-md-8 col-lg-9">
-                    <input name="nama" id="nama" class="form-select" value="<?php echo $_SESSION['nama'];?>" aria-label="Default select example">
+                <label for="kode" class="col-md-4 col-lg-3 col-form-label">Nama Karyawan</label>
+                 <div class="col-md-8 col-lg-6">
+                    <select name="nama" id="nama" class="form-select" aria-label="Default select example">
+                              <option value=""> --- Pilih Karyawan --- </option>
+                              <?php 
+                    $query2="select * from tb_karyawan order by nama";
+                    $tampil=mysqli_query($koneksi, $query2) or die(mysqli_error());
+                    while($data1=mysqli_fetch_array($tampil))
+                    {
+                    ?>
+                              
+							      <option value="<?php echo $data1['nama'];?>"><?php echo $data1['id_karyawan'];?> - <?php echo $data1['nama'];?></option>
+						    <?php } ?>
+                              
+                    </select> 
                   </div>
                 </div>
-
                 <div class="row mb-3">
                   <label for="inputDate" class="col-md-4 col-lg-3 col-form-label">Tanggal</label>
-                  <div class="col-md-8 col-lg-9">
+                  <div class="col-md-8 col-lg-6">
                     <input type="date" class="form-control" name="tanggal" id="tanggal">
                   </div>
                 </div>
 
                 <div class="row mb-3">
                   <label for="inputTime" class="col-md-4 col-lg-3 col-form-label">Jam</label>
-                  <div class="col-md-8 col-lg-9">
-                    <input type="time"  name="jammasuk" id="jammasuk" class="form-control">
-                    <input type="hidden"  name="jam2" id="jam2" class="form-control">
+                  <div class="col-md-8 col-lg-6">
+                    <input type="time" name="jammasuk" id="jammasuk" class="form-control">
+                    <input type="hidden" name="jam2" id="jam2" class="form-control">
                   </div>
                 </div>
 
@@ -111,7 +150,7 @@
 
                 <div class="row mb-3">
                   <label class="col-md-4 col-lg-3 col-form-label">Submit</label>
-                  <div class="col-md-8 col-lg-9">
+                  <div class="col-md-8 col-lg-6">
                   <input type="submit" name="simpan" value="Simpan" class="btn btn-sm btn-primary" />&nbsp;
 	                <a href="inputabsen.php" class="btn btn-sm btn-danger">Batal </a>
                   </div>
@@ -173,5 +212,4 @@
       </script> -->
 
 </body>
-
 </html>
