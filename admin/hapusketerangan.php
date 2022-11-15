92cc35a5657e9c6a8b691a4d162e39dde11736d2
afifmustofa <114654020+afifmustofa@users.noreply.github.com>
2022-11-15 11:37:49 +0700

    Update hapusketerangan.php

--- admin/hapusketerangan.php
+++ admin/hapusketerangan.php
@@ -6,7 +6,7 @@ include('koneksi.php');
 //get id
 $id = $_GET['id'];
 
-$query = "DELETE FROM tb_keterangan WHERE id_keterangan = '$id'";
+$query = "DELETE FROM tb_keterangan WHERE id_ket = '$id'";
 
 if($koneksi->query($query)) {
   echo "<script>alert('Data Keterangan $nama berhasil dihapus!'); window.location = 'dataketerangan.php'</script>";	
