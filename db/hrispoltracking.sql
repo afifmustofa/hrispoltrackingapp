b05f4c089c2230c1bb439cde75770a74fecbd3fb
afifmustofa <114654020+afifmustofa@users.noreply.github.com>
2022-11-25 16:42:15 +0700

    Update hrispoltracking.sql

--- db/hrispoltracking.sql
+++ db/hrispoltracking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 14, 2022 at 07:24 AM
+-- Generation Time: Nov 25, 2022 at 10:38 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -123,6 +123,15 @@ CREATE TABLE `tb_absen` (
   `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tb_absen`
+--
+
+INSERT INTO `tb_absen` (`id_absen`, `nama`, `tanggal`, `jammasuk`, `jam2`, `status`, `image`) VALUES
+('P3019', 'Karyawan 1', '2022-11-17', '14:58', '', '', ''),
+('P7071', 'Karyawan 1', '15-11-2022', '09:31:18', '0931', '', ''),
+('P8757', 'Karyawan 1', '16-11-2022', '04:56:36', '0456', '', '');
+
 -- --------------------------------------------------------
 
 --
@@ -170,9 +179,7 @@ CREATE TABLE `tb_cuti` (
 --
 
 INSERT INTO `tb_cuti` (`kode`, `nama`, `tanggal_awal`, `tanggal_akhir`, `jumlah`, `jenis_cuti`, `ket`, `jam_masuk`, `tanggal`) VALUES
-('CT2927', 'Karyawan', '2022-11-09', '2022-11-10', '2', 'Cuti Tahunan', 'tua', '08:08 am', '09-11-2022'),
-('CT5306', 'Karyawan', '2022-11-10', '2022-11-11', '11', 'Cuti Tahunan', 'reuiree', '08:31 am', '09-11-2022'),
-('CT7409', 'Karyawan', '2022-11-07', '2022-11-08', '2', 'Cuti Tahunan', 'reta', '09:00 am', '07-11-2022');
+('CT9218', 'Ahmad Al Afif', '2022-11-18', '2022-11-18', '1', 'Cuti Tahunan', 'Istri Sakit', '10:35 am', '18-11-2022');
 
 -- --------------------------------------------------------
 
@@ -189,6 +196,13 @@ CREATE TABLE `tb_file` (
   `berkas` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tb_file`
+--
+
+INSERT INTO `tb_file` (`id_file`, `nama`, `title`, `size`, `ekstensi`, `berkas`) VALUES
+('file8976', 'Ahmad Al Afif', 'Komaruddin komaruddin.pdf', '55141', 'pdf', 'file/Komaruddin komaruddin.pdf');
+
 -- --------------------------------------------------------
 
 --
@@ -196,6 +210,100 @@ CREATE TABLE `tb_file` (
 --
 
 CREATE TABLE `tb_karyawan` (
+  `id_karyawan` varchar(10) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `divisi` varchar(50) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `status` enum('TETAP','PKWT','PKWTT') NOT NULL,
+  `agama` text NOT NULL,
+  `jen_kel` text NOT NULL,
+  `tanggal_lahir` text NOT NULL,
+  `pendidikan` varchar(100) NOT NULL,
+  `karkel` text NOT NULL,
+  `npwp` text NOT NULL,
+  `alamat` text NOT NULL,
+  `domisili` text NOT NULL,
+  `nohp` text NOT NULL,
+  `email` text NOT NULL,
+  `statusnikah` text NOT NULL,
+  `kontakkeluarga` text NOT NULL,
+  `jumlah_cuti` varchar(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `level` enum('Admin','Superuser','User') NOT NULL,
+  `gambar` text NOT NULL,
+  `posisi` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id_karyawan`, `nik`, `nama`, `tanggal_masuk`, `divisi`, `jabatan`, `status`, `agama`, `jen_kel`, `tanggal_lahir`, `pendidikan`, `karkel`, `npwp`, `alamat`, `domisili`, `nohp`, `email`, `statusnikah`, `kontakkeluarga`, `jumlah_cuti`, `username`, `password`, `level`, `gambar`, `posisi`) VALUES
+('K4342', '1231', 'Ahmad Al Afif ', '2022-11-18', 'Accounting', 'Direktur', 'TETAP', 'Islam', 'Laki-Laki', '2022-11-18', 'S1', '12313', '123', 'Depok', 'Depok', '08070723424', 'afif@gmail.com', 'Menikah - Belum Punya Anak', '3224234', '12', 'afif', '8cb2237d0679ca88db6464eac60da96345513964', 'User', 'foto_karyawan/4.-Ahmad-Al-Afif-1536x1536.jpg', 'Aktif'),
+('K5507', '23141144', 'Aditya Huda', '2022-11-23', 'Accounting', 'Staff', 'TETAP', 'Islam', 'Laki-Laki', '1995-11-23', 'SMA', '12124411', '1245783', 'Depok', 'Depok', '08123456789', 'adit@email.com', 'Belum Menikah', '08123456789', '12', 'adit', '8cb2237d0679ca88db6464eac60da96345513964', 'User', 'foto_karyawan/adityahuda.jpg', 'Aktif'),
+('K8706', '12342313', 'Karyawan 1', '2022-10-24', 'Accounting', 'Staff', 'PKWT', 'Islam', 'Perempuan', '2000-10-24', 'D3', '-', '-', '-', '-', '-', '-', 'Belum Menikah', '-', '12', 'karyawan', '87c78b8da768468c4f3826791496385536c11dad', 'User', 'foto_karyawan/admin2.jpg', 'Aktif'),
+('K9909', '5566412', 'karyawan 2', '2022-11-10', 'Accounting', 'Staff', 'TETAP', 'Islam', 'Perempuan', '1995-11-03', 'S2', '222111122111', '222111122111', 'depok', 'depok', '081300990099', 'k@email.com', 'Belum Menikah', '1', '12', 'karyawan2', '87c78b8da768468c4f3826791496385536c11dad', 'User', 'foto_karyawan/profileimg.jpg', 'Non - Aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kel`
+--
+
+CREATE TABLE `tb_kel` (
+  `id_karyawan` varchar(10) NOT NULL,
+  `namakel` varchar(100) NOT NULL,
+  `hubungan` varchar(10) NOT NULL,
+  `anak1` varchar(100) NOT NULL,
+  `anak2` varchar(100) NOT NULL,
+  `anak3` varchar(200) NOT NULL,
+  `anak4` varchar(100) NOT NULL,
+  `nohp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_kel`
+--
+
+INSERT INTO `tb_kel` (`id_karyawan`, `namakel`, `hubungan`, `anak1`, `anak2`, `anak3`, `anak4`, `nohp`) VALUES
+('K4342', 'Apri Yanti', 'Istri', '', '', '', '', '083131313131'),
+('K5507', 'Huda', 'Istri', '', '', '', '', '123'),
+('K9909', 'Mardi', 'Ayah', '-', '-', '-', '-', '087788787');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keterangan`
+--
+
+CREATE TABLE `tb_keterangan` (
+  `id_ket` varchar(10) NOT NULL,
+  `nama` text NOT NULL,
+  `tanggal` text NOT NULL,
+  `jam_masuk` text NOT NULL,
+  `ket` text NOT NULL,
+  `alasan` text NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_keterangan`
+--
+
+INSERT INTO `tb_keterangan` (`id_ket`, `nama`, `tanggal`, `jam_masuk`, `ket`, `alasan`, `status`) VALUES
+('Ket3707', 'Karyawan 1', '2022-11-15', '10:33', 'Izin', 'tuyuop', ''),
+('Ket9339', 'Karyawan', '07-11-2022', '08:59 am', 'Izin', 'wewro', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_nonaktif`
+--
+
+CREATE TABLE `tb_nonaktif` (
   `id_karyawan` varchar(10) NOT NULL,
   `nik` varchar(16) NOT NULL,
   `nama` varchar(100) NOT NULL,
@@ -219,38 +327,8 @@ CREATE TABLE `tb_karyawan` (
   `password` text NOT NULL,
   `level` enum('Admin','Superuser','User') NOT NULL,
   `gambar` text NOT NULL,
-  `cvkar` varchar(100) NOT NULL
+  `tgl_arsip` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_karyawan`
---
-
-INSERT INTO `tb_karyawan` (`id_karyawan`, `nik`, `nama`, `tanggal_masuk`, `divisi`, `jabatan`, `status`, `agama`, `jen_kel`, `tanggal_lahir`, `karkel`, `npwp`, `alamat`, `domisili`, `nohp`, `email`, `statusnikah`, `kontakkeluarga`, `jumlah_cuti`, `username`, `password`, `level`, `gambar`, `cvkar`) VALUES
-('K8706', '12342313', 'Karyawan 1', '2022-10-24', 'General Affairs', 'Staff', 'TETAP', 'Islam', 'Laki - Laki', '2000-10-24', '-', '-', '-', '-', '-', '-', 'TK-0', '-', '12', 'karyawan', '87c78b8da768468c4f3826791496385536c11dad', 'User', 'foto_karyawan/profileimg.jpg', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_keterangan`
---
-
-CREATE TABLE `tb_keterangan` (
-  `id_ket` varchar(10) NOT NULL,
-  `nama` text NOT NULL,
-  `tanggal` text NOT NULL,
-  `jam_masuk` text NOT NULL,
-  `ket` text NOT NULL,
-  `alasan` text NOT NULL,
-  `status` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tb_keterangan`
---
-
-INSERT INTO `tb_keterangan` (`id_ket`, `nama`, `tanggal`, `jam_masuk`, `ket`, `alasan`, `status`) VALUES
-('Ket9339', 'Karyawan', '07-11-2022', '08:59 am', 'Izin', 'wewro', '');
 
 --
 -- Indexes for dumped tables
@@ -311,10 +389,22 @@ ALTER TABLE `tb_karyawan`
   ADD PRIMARY KEY (`id_karyawan`);
 
 --
+-- Indexes for table `tb_kel`
+--
+ALTER TABLE `tb_kel`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
 -- Indexes for table `tb_keterangan`
 --
 ALTER TABLE `tb_keterangan`
   ADD PRIMARY KEY (`id_ket`);
+
+--
+-- Indexes for table `tb_nonaktif`
+--
+ALTER TABLE `tb_nonaktif`
+  ADD PRIMARY KEY (`id_karyawan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
