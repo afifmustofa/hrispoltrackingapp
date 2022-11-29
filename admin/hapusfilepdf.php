cf0e990d08af5ad0182e1af0bab0ae8a8084eabc
afifmustofa <114654020+afifmustofa@users.noreply.github.com>
2022-11-29 11:10:12 +0700

    Update hapusfilepdf.php

--- admin/hapusfilepdf.php
+++ admin/hapusfilepdf.php
@@ -6,7 +6,7 @@ include('koneksi.php');
 //get id
 $id = $_GET['id'];
 
-$query = "DELETE FROM tb_file WHERE id_karyawan = '$id'";
+$query = "DELETE FROM tb_file WHERE id_file = '$id'";
 
 if($koneksi->query($query)) {
   echo "<script>alert('Data berhasil dihapus!'); window.location = 'datafile.php'</script>";	
